fee6138e72091ec475e4068c170c49aa3336954b
WhiteHeadbanger <white.seba@hotmail.com>
2024-04-19 04:47:21 -0300

    added logic and comparisons
    Added:
    * ==
    * >
    * <
    * >=
    * <=
    * y (and)
    * o (or)
    * truchar (not)
    
    Also added:
    * nada (null, None)
    * posta (true)
    * trucho (false)

--- src/interpreter.py
+++ src/interpreter.py
@@ -88,6 +88,30 @@ class Interpreter:
         
         elif node.op_tok.type == TT_POW:
             result, error = left.powered_by(right)
+        
+        elif node.op_tok.type == TT_EE:
+            result, error = left.get_comparison_eq(right)
+        
+        elif node.op_tok.type == TT_NE:
+            result, error = left.get_comparison_ne(right)
+        
+        elif node.op_tok.type == TT_LT:
+            result, error = left.get_comparison_lt(right)
+        
+        elif node.op_tok.type == TT_GT:
+            result, error = left.get_comparison_gt(right)
+        
+        elif node.op_tok.type == TT_LTE:
+            result, error = left.get_comparison_lte(right)
+        
+        elif node.op_tok.type == TT_GTE:
+            result, error = left.get_comparison_gte(right)
+        
+        elif node.op_tok.matches(TT_KEYWORD, 'y'):
+            result, error = left.anded_by(right)
+        
+        elif node.op_tok.matches(TT_KEYWORD, 'o'):
+            result, error = left.ored_by(right)
 
         if error:
             return res.failure(error)
@@ -104,6 +128,8 @@ class Interpreter:
 
         if node.op_tok.type == TT_MINUS:
             number, error = number.multiplied_by(Number(-1))
+        elif node.op_tok.matches(TT_KEYWORD, 'truchar'):
+            number, error = number.notted()
 
         if error:
             return res.failure(error)
