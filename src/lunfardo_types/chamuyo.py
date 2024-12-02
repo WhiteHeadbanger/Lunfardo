be39453124e0dd24afac35263f9de55218f53ae7
andresbonelli <andresbonellipiano@gmail.com>
2024-12-02 16:58:08 -0300

    refactor: Rename custom bool type class "Boolean"to "Boloodean"

--- src/lunfardo_types/chamuyo.py
+++ src/lunfardo_types/chamuyo.py
@@ -1,6 +1,7 @@
 from .value import Value
 from .numero import Numero
-from .boolean import Boolean
+from .boloodean import Boloodean
+
 
 class Chamuyo(Value):
 
@@ -11,36 +12,36 @@ class Chamuyo(Value):
     def added_to(self, other):
         if isinstance(other, Chamuyo):
             return Chamuyo(self.value + other.value).set_context(self.context), None
-        
+
         return None, Value.illegal_operation(self, other)
-    
+
     def multiplied_by(self, other):
         if isinstance(other, Numero):
             return Chamuyo(self.value * other.value).set_context(self.context), None
-        
+
         return None, Value.illegal_operation(self, other)
-    
+
     def get_comparison_eq(self, other):
         if isinstance(other, (Chamuyo, Numero)):
-            return Boolean(self.value == other.value).set_context(self.context), None
-    
+            return Boloodean(self.value == other.value).set_context(self.context), None
+
         return None, Value.illegal_operation(self, other)
-    
+
     def get_comparison_ne(self, other):
         if isinstance(other, (Chamuyo, Numero)):
-            return Boolean(self.value != other.value).set_context(self.context), None
-    
+            return Boloodean(self.value != other.value).set_context(self.context), None
+
         return None, Value.illegal_operation(self, other)
-    
+
     def is_true(self):
-        return Boolean(len(self.value) > 0).set_context(self.context), None
-    
+        return Boloodean(len(self.value) > 0).set_context(self.context), None
+
     def copy(self):
         copy = Chamuyo(self.value)
         copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
         return copy
-    
+
     def __str__(self):
         return f'"{self.value}"'
 
