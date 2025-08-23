10facc68d35518372e7c0281c22055ccfbd3ff0c
WhiteHeadbanger <white.seba@hotmail.com>
2025-08-22 21:51:16 -0300

    chore: fixed chusmea frame drawing alignment

--- src/chusma.py
+++ src/chusma.py
@@ -16,22 +16,22 @@ class Chusma:
         self.res.register(self.matear_func.execute([title], self.context, self))
         for frame_name, context_for_frame in zip(self.get_stack(), self.get_context_stack()):
             # :<40 -> Formats the string to a 40-character width, padding with spaces on the right.
-            frame_str = Chamuyo(f"│ Frame: {str(frame_name):<40}│")
+            frame_str = Chamuyo(f"│ Frame: {str(frame_name):<39}│")
             self.res.register(self.matear_func.execute([frame_str], self.context, self))
 
             raw_symbols: dict = context_for_frame.symbol_table.symbols
 
             # print symbols
             for sym, value in raw_symbols.items():
-                self.res.register(self.matear_func.execute([Chamuyo(f'│   {sym:<12} → {str(value):<30}│')], self.context, self))
+                self.res.register(self.matear_func.execute([Chamuyo(f'│   {sym:<12} → {str(value):<29}│')], self.context, self))
 
         # print end
         self.res.register(self.matear_func.execute([Chamuyo("│                                               │")], self.context, self))
-        self.res.register(self.matear_func.execute([Chamuyo("│ [Presioná Enter para seguir]                   │")], self.context, self))
-        self.res.register(self.matear_func.execute([Chamuyo("└──────────────────────────────────────────────┘")], self.context, self))
+        self.res.register(self.matear_func.execute([Chamuyo("│ [Presioná Enter para seguir]                  │")], self.context, self))
+        self.res.register(self.matear_func.execute([Chamuyo("└───────────────────────────────────────────────┘")], self.context, self))
         
         morfar_func = self.context.symbol_table.get("morfar")
-        morfar_value = self.res.register(morfar_func.execute([], self.context, self))
+        self.res.register(morfar_func.execute([], self.context, self))
 
 
     def get_stack(self):
